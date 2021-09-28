113fe20ed650ee05ad68277562242dd9a6a5343e
seadiv <animesh1836@gmail.com>
2021-09-28 17:00:41 +0530

    second commit

--- app/src/main/java/com/example/dspcalculatorandnotes/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/dspcalculatorandnotes/ui/slideshow/SlideshowFragment.java
@@ -4,32 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.dspcalculatorandnotes.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
 
 public class SlideshowFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
 
+    //String url="https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCsFDZ6Gv1SeOaT7YBQsn3bQ&maxResults=3&key=AIzaSyCISoTEFc7oowlIaxoCx9FtDULpDFU3Sso";
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        View view=inflater.inflate(R.layout.fragment_slideshow,container,false);
+
+        return view;
+
     }
 }
